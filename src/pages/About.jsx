e23aea23ff7381f6b06bb9fd2822b010adf79773
Olubodun Adeola <olubodunadeola2000@gmail.com>
2025-11-30 15:38:55 +0100

    feat(style): add screen brakpoint

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     const { theme } = useContext(ThemeContext);
 
     return (
-        <div className={`min-h-screen p-6 transition-colors ${theme === "light" ? "bg-white text-gray-900" : "bg-gray-900 text-gray-100"}`}>
+        <div className={`min-h-screen md:w-4xl p-6 transition-colors ${theme === "light" ? "bg-white text-gray-900" : "bg-gray-900 text-gray-100"}`}>
             <h1 className="text-2xl font-bold mb-4">About</h1>
             <div className={`p-5 rounded-xl shadow ${theme === "light" ? "bg-gray-100" : "bg-gray-800"}`}>
                 <p className="leading-7">
--- src/pages/Legal.jsx
+++ src/pages/Legal.jsx
@@ -5,7 +5,7 @@ export default function Legal() {
     const { theme } = useContext(ThemeContext);
 
     return (
-        <div className={`min-h-screen p-6 transition-colors ${theme === "light" ? "bg-white text-gray-900" : "bg-gray-900 text-gray-100"} `}>
+        <div className={`min-h-screen md:w-4xl p-6 transition-colors ${theme === "light" ? "bg-white text-gray-900" : "bg-gray-900 text-gray-100"} `}>
             <h1 className="text-2xl font-bold mb-4">Legal</h1>
             <div className={`p-5 rounded-xl shadow ${theme === "light" ? "bg-gray-100" : "bg-gray-800"}`}>
                 <p className="leading-7 mb-3">This is for learning and educational purposes. All features and content are intended for demonstration only.</p>
