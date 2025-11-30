933a44f82dd86268e4a4250b9df6d3278902c542
Olubodun Adeola <olubodunadeola2000@gmail.com>
2025-11-30 14:43:59 +0100

    feat: style Legal page

--- src/pages/Legal.jsx
+++ src/pages/Legal.jsx
@@ -1,3 +1,17 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
 export default function Legal() {
-    return <h1 className="text-center mt-10">Legal</h1>;
+    const { theme } = useContext(ThemeContext);
+
+    return (
+        <div className={`min-h-screen p-6 transition-colors ${theme === "light" ? "bg-white text-gray-900" : "bg-gray-900 text-gray-100"} `}>
+            <h1 className="text-2xl font-bold mb-4">Legal</h1>
+            <div className={`p-5 rounded-xl shadow ${theme === "light" ? "bg-gray-100" : "bg-gray-800"}`}>
+                <p className="leading-7 mb-3">This is for learning and educational purposes. All features and content are intended for demonstration only.</p>
+                <p className="text-sm opacity-75">© {new Date().getFullYear()} TeluadOat. All rights reserved</p>
+            </div>
+        </div>
+
+    )
 }
